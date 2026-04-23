adb73aa30fa581ad7d9226040ccdf7c863ea5a8d
Shafqat Ullah <shafqat@sourcevo.com>
2026-04-24 00:15:59 +1000

    [RED] test(wave-4): scaffold escape test stubs for kay-sandbox-{macos,linux,windows}
    Signed-off-by: Shafqat Ullah <shafqat@sourcevo.com>

---
+++ crates/kay-sandbox-windows/tests/escape.rs
@@ -0,0 +1,7 @@
+#[cfg(target_os = "windows")]
+#[test]
+fn sandbox_blocks_write_outside_allowed_dir() { todo!() }
+
+#[cfg(target_os = "windows")]
+#[test]
+fn sandbox_allows_write_inside_allowed_dir() { todo!() }
